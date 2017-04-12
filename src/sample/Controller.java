22c1547c5e58f2850e5c6a7e160463f3a7932f79
JurateJuo <defliunke@gmail.com>
2017-04-12 21:00:46 +0300

    6 paskaitos 1.2 uzdavinys

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,17 +1,35 @@
 package sample;
 
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.util.Random;
 
 public class Controller {
-
     @FXML
-    private Label text;
+    private Label skaicius;
+    @FXML
+    private TextField spejimas;
 
-    public void sayHi(ActionEvent event){
-        String prefix = text.getText();
-        text.setText(prefix + " Jurate");
+    public void speti(ActionEvent e) {
+        Random random = new Random();
+        int x = random.nextInt(10)+1;
+        int y = Integer.parseInt(spejimas.getText());
+        if (x==y){
+            skaicius.setText("Saunuole");
+        }else {
+            skaicius.setText("Neatspejai :P " + x);
+        }
+
     }
 
+    public void baigti(ActionEvent b) {
+        System.exit(0);
+
+    }
 }
+
+
